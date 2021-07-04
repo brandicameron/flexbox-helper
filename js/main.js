aa91b3ec37822b4aea7b64cb38a1c6244b084517
Brandi Cameron <hello@brandicameron.com>
2021-07-04 15:36:11 -0500

    added current settings update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
 gsap.registerPlugin(Flip);
 
-const controls = document.querySelector(".controls");
+const controls = document.querySelector('.controls');
 
-function flex(e) {
-  const flexContainer = document.querySelector(".container");
-  const state = Flip.getState(".item");
-  const settings = document.querySelector(".settings");
-
-  let flexProp = e.target.parentElement.dataset.prop;
+function changeFlexValues(e) {
+  const flexContainer = document.querySelector('.container');
+  const state = Flip.getState('.item');
+  const flexProp = e.target.parentElement.dataset.prop;
   flexContainer.style[flexProp] = e.target.value;
-
-  // if (flexProp === "justifyContent") {
-  //   settings.textContent = `justify-content: ${e.target.value}`;
-  // }
 
   Flip.from(state, {
     absolute: true,
     duration: 0.4,
-    ease: "sine",
+    ease: 'sine',
   });
+
+  displayCurrentSettings(e, flexProp);
 }
 
-controls.addEventListener("click", flex);
+function displayCurrentSettings(e, property) {
+  let currentFlexDirection = document.querySelector('.flex-direction');
+  let currentJustifyContent = document.querySelector('.justify-content');
+  let currentAlignItems = document.querySelector('.align-items');
+
+  switch (property) {
+    case 'flexDirection':
+      currentFlexDirection.textContent = `flex-direction: ${e.target.value};`;
+      break;
+    case 'justifyContent':
+      currentJustifyContent.textContent = `justify-content: ${e.target.value};`;
+      break;
+    case 'alignItems':
+      currentAlignItems.textContent = `align-items: ${e.target.value};`;
+      break;
+  }
+}
+
+controls.addEventListener('click', changeFlexValues);
